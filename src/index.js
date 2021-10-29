2809a04cbba27faae57f1699725ea92b9783c9af
mugabe richard <mugabe.richard.37@gmail.com>
2021-10-29 09:15:51 +0200

    Final Project Commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import ReactDOM from 'react-dom';
 import './assets/main.css';
 import App from './App';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
